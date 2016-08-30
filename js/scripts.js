d5f643605c973887c02e75acd2150bc504c0be10
lukeslattry <lukeslattery@ymail.com>
2016-08-30 18:39:38 +0100

    Redesign

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,22 @@
 (function() {
     'use strict';
     document.addEventListener("DOMContentLoaded", function() {
-        var imgs = document.getElementsByTagName("img");
-        imagesLoaded(imgs).on( 'progress', function( instance, image ) {
-            image.img.className = "visible";
-        });
+        var items = document.querySelectorAll('.item');
+        function itemHeight() {
+            for (var i = items.length; i--;) {
+                items[i].style.padding = '0px';
+                items[i].style.height = items[i].offsetWidth * 0.625 + 'px';
+            }
+        }
+        itemHeight();
+        window.addEventListener('resize', itemHeight, false);
+        var itemNames = document.querySelectorAll('.item-name');
+        document.addEventListener('mousemove', titles, false);
+        function titles(e) {
+            for (var i = itemNames.length; i--;) {
+                itemNames[i].style.left = e.pageX - itemNames[i].offsetWidth / 2 + 'px';
+                itemNames[i].style.top = e.pageY - 36 + 'px';
+            }
+        }
     });
 })();
